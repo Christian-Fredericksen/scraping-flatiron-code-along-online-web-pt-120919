b8c3c22c823b8de8fcf53f1da93c570b215bba81
Christian Fredericksen <christian.fredericksen@yahoo.com>
2020-01-28 01:08:54 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,16 +6,16 @@ require_relative './course.rb'
 
 class Scraper
   
-  def print_courses
-    self.make_courses
-    Course.all.each do |course|
-      if course.title && course.title != ""
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
-      end
-    end
-  end
+  # def print_courses
+  #   self.make_courses
+  #   Course.all.each do |course|
+  #     if course.title && course.title != ""
+  #       puts "Title: #{course.title}"
+  #       puts "  Schedule: #{course.schedule}"
+  #       puts "  Description: #{course.description}"
+  #     end
+  #   end
+  # end
   
   
   def get_page
@@ -23,7 +23,7 @@ class Scraper
     binding.pry 
   end 
   
-  
+ Scraper.new.get_page 
   
   
 end
